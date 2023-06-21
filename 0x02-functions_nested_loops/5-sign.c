f0e97236db923f63b195bed51984e839532af873
Byronn Coetzee <byronncoetzee@gmail.com>
2023-06-20 17:56:34 -0700

    hoop

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: the number thats checked
+ * Return: 1 for positive, -1 for negative, and zero for everything else
+ */
+
+int print_sign(int n)
+
+{
+
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchhar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+}
