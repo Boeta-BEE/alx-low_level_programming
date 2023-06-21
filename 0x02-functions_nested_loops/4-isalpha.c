757d655e682e433787a303b3c2581356b5b7dcde
Byronn Coetzee <byronncoetzee@gmail.com>
2023-06-20 17:44:09 -0700

    joh

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 
 /**
- * _isaplha - Checks for alphabetic character
+ * _isalpha - Checks for alphabetic character
  * @c: the specific character thats checked
  * Return: 1 fot alphabet, 0 for anything else
  */
